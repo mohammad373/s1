1a5d2c260fd9a5d319e409107788666b5e955065
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2021-01-04 17:50:23 +0330

    Update s1.py

--- s1.py
+++ s1.py
@@ -21,7 +21,7 @@ def __target__():
         except:
             sys.exit()
     r1 = requests.get("https://api.hackertarget.com/whois/?q=" + target)
-    if r.status_code != 200:
+    if r1.status_code != 200:
         try:
             time.sleep(1)
             print(Fore.RED + "\n[!] ~ Error : Your Domain Is Not Found ;(")
